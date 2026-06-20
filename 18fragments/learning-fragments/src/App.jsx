152e5167c7743ef6086f00a0cce7fb511f28fdc2
Dishant Girase <dishantgirase6@gmail.com>
2026-06-20 16:47:18 +0530

    19/06/2026

--- 18fragments/learning-fragments/src/App.jsx
+++ 18fragments/learning-fragments/src/App.jsx
@@ -1,14 +1,17 @@
+import Style from "./coponents/Heading.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Buffering from "./coponents/buffering";
 import DataShowing from "./coponents/dataShowing";
+import Heading from "./coponents/heading";
+
 function App() {
   let foodsArr = ["samosa", "idli", "vada", "khaman", "khichdi", "dal"];
   // let foodsArr = [];
 
   return (
     <>
-      <h2>foods</h2>
+      <Heading className={`${Style.headingCss}`}></Heading>
       {foodsArr.length === 0 ? <Buffering></Buffering> : null}
       {foodsArr.map((item) => (
         <ul className="list-group">
---
+++ 18fragments/learning-fragments/src/coponents/heading.jsx
@@ -0,0 +1,4 @@
+const Heading = () => {
+  return <h1>FOODS</h1>;
+};
+export default Heading;
